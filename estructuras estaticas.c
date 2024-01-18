1389320f10c12177a24d8d2696d393ab4cd6a6ea
SantuuGaray999 <156965886+SantuuGaray999@users.noreply.github.com>
2024-01-18 11:04:24 -0300

    Add files via upload

---
+++ estructuras estaticas.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[3][3] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
+    };
+
+    // Mostrar la matriz identidad
+    printf("Matriz Identidad:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
